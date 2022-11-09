6341b0cebac7ed0a4a4b6832f8371ffd9f17f7bf
xxalison <109158990+xxalisonn@users.noreply.github.com>
2022-11-09 14:06:38 +0800

    Update models.py

--- models.py
+++ models.py
@@ -152,7 +152,7 @@ class MetaP(nn.Module):
                         temp_score += torch.norm(query[i][j] - self.proto[t],2,-1)
                 score[i][j] = - temp_score / (len(self.proto)-1)
 
-        return score
+        return score.cuda()
 
     def forward(self, task, iseval=False, curr_rel="", select=False, use_conv=False):
         # transfer task string into embedding
